5a9c59c817ff1f5b6476187f48c057956b6d6db3
leimiles <leimiles_18@hotmail.com>
2025-10-27 23:57:35 +0800

    <feature> save settings

--- Assets/Settings/Scripts/WXSettings.cs
+++ Assets/Settings/Scripts/WXSettings.cs
@@ -5,7 +5,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WXSettings", menuName = "Generate/WXSettings", order = 1)]
 public class WXSettings : ScriptableObject
 {
-    public string appId = "your_app_id_here";
-    public string cdnUrl = "https://your.cdn.url/here/";
-    public string appName = "YourAppName";
+    public enum FirstBundleLoadingMethodEnum
+    {
+        CDN,
+        Local,
+    }
+    public string appId = "wxc3fc029bfde9435b";
+    public string cdnUrl = "https://a.unity.cn/client_api/v1/buckets/be845f55-2cfe-4e08-87e9-1b8162181a67/release_by_badge/dev_01_h/content";
+    public string appName = "朱麦历险记";
+    public FirstBundleLoadingMethodEnum firstBundleLoadingMethod = FirstBundleLoadingMethodEnum.CDN;
+    public int firstBundleCompress = 1;
+    public int preloadWXFont = 0;
+    public int WXMultiTouchForbidden = 1;
+
 }
