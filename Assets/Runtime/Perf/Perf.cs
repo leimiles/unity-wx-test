0abe0b3f6903b45febbdbd9eb585c41bcc0632b3
leimiles <leimiles_18@hotmail.com>
2025-10-29 00:39:44 +0800

    <feature> add perf func

--- Assets/Runtime/Perf/Perf.cs
+++ Assets/Runtime/Perf/Perf.cs
@@ -1,7 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WeChatWASM;
 
 public class Perf : MonoBehaviour
 {
+    void Start()
+    {
+        ReportSceneOption option = new ReportSceneOption();
+        option.sceneId = 1000;
+
+        Debug.Log("option: " + option.sceneId);
+
+
+    }
 }
