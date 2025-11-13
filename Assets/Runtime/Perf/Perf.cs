150339ec4b635b85561a5fbf099f6b035954efa3
zhulei <>
2025-11-13 19:11:04 +0800

    <feature> testing yoo asset ab

--- Assets/Runtime/Perf/Perf.cs
+++ Assets/Runtime/Perf/Perf.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_WEBGL
 using WeChatWASM;
+#endif
 
 public class Perf : MonoBehaviour
 {
     void Start()
     {
+#if UNITY_WEBGL
         WX.ReportGameStart();
+#endif
     }
 }
