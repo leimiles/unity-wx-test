336e74b164c77eef8f662cd455aa6bb57e417aad
zhulei <zhulei@xmfunny.com>
2025-11-27 17:47:44 +0800

    <feature> add wx memory api

--- Assets/Runtime/Perf/Perf.cs
+++ Assets/Runtime/Perf/Perf.cs
@@ -16,6 +16,49 @@ public class Perf : MonoBehaviour
     {
 #if UNITY_WEBGL
         WX.ReportGameStart();
+        WX.OnMemoryWarning(OnMemoryWarningCallback);
+#endif
+    }
+
+    void OnMemoryWarningCallback(OnMemoryWarningListenerResult result)
+    {
+        Debug.Log($"[WX] Memory Warning Triggered! Level: {result.level}");
+    }
+
+    public void PeekWXMemory()
+    {
+#if UNITY_WEBGL
+
+        // ----------- WeChat Mini Game 内存信息 ----------
+        // 会报错，先关闭
+        // float totalStackSize = WX.GetTotalStackSize();
+        // Debug.Log($"[WX] Total Stack Size: {totalStackSize}");
+
+        float totalMemory = WX.GetTotalMemorySize();
+        Debug.Log($"[WX] Total Memory Size: {totalMemory / 1048576f:F1} MB");
+
+        // 拿不到数据，但是不会报错
+        float usedMemory = WX.GetUsedMemorySize();
+        Debug.Log($"[WX] Used Memory Size: {usedMemory / 1048576f:F1} MB");
+
+        // 会报错，先关闭
+        // float staticMemorySize = WX.GetStaticMemorySize();
+        // Debug.Log($"[WX] Static Memory Size: {staticMemorySize / 1048576f:F1} MB");
+
+        float dynamicMemorySize = WX.GetDynamicMemorySize();
+        Debug.Log($"[WX] Dynamic Memory Size: {dynamicMemorySize / 1048576f:F1} MB");
+
+        float unAllocatedMemorySize = WX.GetUnAllocatedMemorySize();
+        Debug.Log($"[WX] Unallocated Memory Size: {unAllocatedMemorySize / 1048576f:F1} MB");
+
+        float bundleSizeInMemory = WX.GetBundleSizeInMemory();
+        Debug.Log($"[WX] Bundle Size In Memory: {bundleSizeInMemory} ");
+
+        float bundleNumberInMemory = WX.GetBundleNumberInMemory();
+        Debug.Log($"[WX] Bundle Number In Memory: {bundleNumberInMemory}");
+
+#else
+        Debug.Log("WeChat Memory probe only works in WeChat WebGL builds.");
 #endif
     }
 
