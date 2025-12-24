d4d202fdaa7ab5991cca99742a69847bff5039cd
Miles Zhu <leimiles_18@hotmail.com>
2025-12-25 00:07:47 +0800

    feat. gameworld service can find gameworld

--- Assets/Runtime/GameWorld/GameWorldService.cs
+++ Assets/Runtime/GameWorld/GameWorldService.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
-using System.Threading;
 using Cysharp.Threading.Tasks;
 public interface IGameWorldService : IDisposable
 {
@@ -14,6 +11,7 @@ public interface IGameWorldService : IDisposable
 
 public class GameWorldService : IGameWorldService
 {
+    private const string GameWorldTag = "GameWorld";
     public bool HasWorld => _currentWorld != null;
     IGameWorld _currentWorld;
     public IGameWorld CurrentWorld => _currentWorld;
@@ -32,7 +30,44 @@ public class GameWorldService : IGameWorldService
 
     void OnGameWorldEnter(GameWorldEnterEvent e)
     {
+        SetCurrentWorld();
         Debug.Log("[GameWorldService] enter game world");
+    }
+
+    /// <summary>
+    /// 设置当前游戏世界，非常严格，必须只有一个GameWorld对象，并且必须实现IGameWorld接口
+    /// </summary>
+    /// <exception cref="InvalidOperationException"></exception>
+    void SetCurrentWorld()
+    {
+        var gos = GameObject.FindGameObjectsWithTag(GameWorldTag);
+
+        if (gos == null || gos.Length == 0)
+            throw new InvalidOperationException($"[GameWorldService] No GameWorld found (tag='{GameWorldTag}').");
+
+        if (gos.Length > 1)
+        {
+            var names = "";
+            for (int i = 0; i < gos.Length; i++)
+            {
+                if (i > 0) names += ", ";
+                names += gos[i].name;
+            }
+
+            throw new InvalidOperationException(
+                $"[GameWorldService] Multiple GameWorld objects found (tag='{GameWorldTag}'): {gos.Length}. Objects: {names}");
+        }
+
+
+        var go = gos[0];
+
+        if (!go.TryGetComponent<IGameWorld>(out var world) || world == null)
+            throw new InvalidOperationException(
+                $"[GameWorldService] GameObject '{go.name}' has tag '{GameWorldTag}' but does not implement IGameWorld.");
+
+        _currentWorld = world;
+
+        Debug.Log($"[GameWorldService] set current game world: '{_currentWorld.Name}' (GO='{go.name}')");
     }
 
     public void Dispose()
